d399de1a0bfb7d49daadaa4bd47b534b4e522234
chandrabhushan-kumar <kumar30895@gmail.com>
2020-06-17 19:28:21 +0530

    Chandra made chanhes in  all sevices block  our speciality

--- webapp/admin/src/theme1/blocks/ListofServices.js
+++ webapp/admin/src/theme1/blocks/ListofServices.js
@@ -19,20 +19,13 @@ export default class ListofServices extends React.Component {
 					</div> */}
 					<div className="col-lg-2 col-lg-offset-5 services"><h1 className="services1">Services</h1></div>
 					<div className="col-lg-8 col-lg-offset-2 Hexagonwrapper">
-					  <div className="hexagon1"><div className="images1"></div><h1 className="eprizeapps">ENTERPRISE APPS</h1></div>
-					  <div className="hexagon2"></div>
-					  <div className="hexagon3"></div>
-					  <div className="hexagon4"></div>
-					  <div className="hexagon5"></div>
-					  <div className="hexagon6"></div>
-					  <div className="hexagon7"></div>
-
-
-
-
-						
-
-						
+						<div className="hexagon1"><div className="images1"></div><h1 className="heprizeapps">ENTERPRISE APPS</h1></div>
+						<div className="hexagon2"><h1 className="h1services">SERVICES</h1></div>
+						<div className="hexagon3"><div className="images2"></div><h1 className="hstaffaugmtion">STAFF AUGMEN-TATION</h1></div>
+						<div className="hexagon4"><div className="images3"></div><h1 className="hwebapps">WEB APPS</h1></div>
+						<div className="hexagon5"><div className="images4"></div><h1 className="hbportals">BUSINESS PORTALS</h1></div>
+						<div className="hexagon6"><div className="images5"></div><h1 className="hmapps">MOBILES APPS</h1></div>
+						<div className="hexagon7"><div className="images6"></div><h1 className="heportal">eCOMMERCE PORTAL</h1></div>
 					</div>
 
 
--- webapp/admin/src/theme1/blocks/OutSpeciality.js
+++ webapp/admin/src/theme1/blocks/OutSpeciality.js
@@ -11,72 +11,74 @@ export default class OutSpeciality extends React.Component {
 		return (
 			<div className="container-fluid nopadding">
 			<div className="OurSpecialityHeight  hidden-sm hidden-xs">
-			<div className="mtop111">	
-				<div className="col-lg-12 col-md-12 hidden-sm hidden-xs bhead">
-					<div className="col-lg-12 col-md-12 hidden-sm hidden-xs ">
+			 <div className="mtop111">	 
+				 <div className="col-lg-12 col-md-12 hidden-sm hidden-xs bhead">
+					 <div className="col-lg-12 col-md-12 hidden-sm hidden-xs ">
 						<ul className="dashBoxOS col-lg-2 col-lg-offset-5 col-md-2 col-md-offset-5">
 						  <li className="dash1OS"></li>
 						  <li className="dash2OS"></li>
 						  <li className="dash3OS"></li>
 						</ul>
-					</div>
-					<div className="text-center">
+					</div> 
+					<div className=" col-lg-2 col-lg-offset-5 mainheading">
 						<h1>
 							<b className="whiteclr">Our Speciality</b>
 						</h1>
 					</div>
-				</div>
-				<div className="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 hidden-sm hidden-xs osDiv">
-					<div className="row">
+				</div> 
+				 <div className="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 hidden-sm hidden-xs osDiv"> 
+					 <div className="row"> 
 						<div className="col-lg-4 col-md-4">
-							<div className="col-lg-12 col-md-12 col1">
-								<div className="iconCircle">
-									<img src="/images/13.png" alt="enteprice" className="innerCircle"/>
-								</div>
-								<div className="sptextBox1">
-									<h2 className="text-center"><b>Enterprise<br/> Apps</b></h2>
-									<div className="col-lg-12 col-md-12">
-										<p>We have indepth expertise in developing large scale Enterprise grade web & mobile apps.
+							 <div className="col-lg-12 col-md-12 col1"> 
+								 <div className="iconCircle">
+									<img src="/images/13.png" alt="enteprice" className="innerCircle"/> 
+								</div> 
+								  <div className="sptextBox1">
+									<h2 className="text-center mheading">Enterprise</h2>
+									 <div className="col-lg-12 col-md-12">
+										<p className="paragraph">We have indepth expertise in developing large scale Enterprise grade web & mobile apps.
 									  	</p>
 									</div>
-									{/*<p className="text-center rmore">Read More</p>*/}
-								</div>
+									<p className="text-center rmore">Read More</p>  
+								</div>   
 
-							</div>
-						</div>
-						<div className="col-lg-4 col-md-4">
+							</div> 
+						 </div> 
+						 <div className="col-lg-4 col-md-4">
 							<div className="col-lg-12 col-md-12 col1">
 								<div className="iconCircle">
 									<img src="/images/14.png" alt="enteprice" className="innerCircle"/>
 								</div>
-								<div className="sptextBox1">
-									<h2 className="text-center"><b>Startup <br/>Specialist</b></h2>
+							 <div className="sptextBox1">
+									<h2 className="text-center mheading">Startup World</h2>
 									<div className="col-xl-12 col-lg-12 col-md-12">
-										<p>Specially made plans for Startups that are highly cost effective & value added services. Our quotations are absolutely unbeatable in the IT Industry.
+										<p className="paragraph">Specially made plans for Startups that are highly cost effective & value added services. Our quotations are absolutely unbeatable in the IT Industry.
 									  </p>
-									</div>
+									</div> 
+									 <p className="text-center rmore">Read More</p>    
+
 									
-								</div>
+								</div> 
 							</div>
-						</div>
-						<div className="col-lg-4 col-md-4">
+						</div> 
+						 <div className="col-lg-4 col-md-4">
 							<div className="col-lg-12 col-md-12 col1">
 								<div className="iconCircle">
 									<img src="/images/15.png" alt="enteprice" className="innerCircle"/>
 								</div>
 								<div className="sptextBox1">
-									<h2 className="text-center"><b>Customised eCommerce</b></h2>
-									<div className="col-xl-12 col-lg-12 col-md-12">
-										<p>Boost Your eCommerce business with our Flexible, Scalable & Robust eCommerce Platform AutoPilot with the blazing fast performance and SEO Friendly Online Stores
+									<h2 className="text-center mheading">eCommerce</h2>
+									 <div className="col-xl-12 col-lg-12 col-md-12">
+										<p className="paragraph">Boost Your eCommerce business with our Flexible, Scalable & Robust eCommerce Platform AutoPilot with the blazing fast performance and SEO Friendly Online Stores
 									  </p>
-									</div>
-									
+									</div> 
+									  <p className="text-center rmore">Read More</p>  
 								</div>
 							</div>
-						</div>
-					</div>
-				</div>
-				</div>
+						</div> 
+					 </div> 
+				 </div> 
+				 </div> 
 			</div>
 {/*====================================== Mobile view ===================================*/}
 			<div className="xsOurSpecialityHeight col-xs-12 col-sm-12 hidden-lg hidden-md container-fluid">

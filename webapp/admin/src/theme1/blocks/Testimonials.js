38162142e9894e590417ee4e8b8d3267b9c0d561
priyanka-kale <kalepriya100@gmail.com>
2020-07-20 22:35:27 +0530

    chandra made changes in carousel issue

--- webapp/admin/src/theme1/blocks/Testimonials.js
+++ webapp/admin/src/theme1/blocks/Testimonials.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Testimonials.css";
 import $ from "jquery";
-// import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 export default class Testimonials extends React.Component {
 
